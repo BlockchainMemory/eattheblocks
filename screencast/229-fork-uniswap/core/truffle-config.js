6c309434a7e56c04bfff4b1df4885efde1a13509
Bub Ridge <bubridge@Bubs-MacBook-Pro.local>
2021-12-02 23:20:58 -0800

    ropsten

--- screencast/229-fork-uniswap/core/truffle-config.js
+++ screencast/229-fork-uniswap/core/truffle-config.js
@@ -18,7 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+// Doxxed Meta 17 0x647f3cdae2bed99ba0295b803a0496d2267c051c8e8960240c6ce158f7f9743f
+const privateKeys = ['0x647f3cdae2bed99ba0295b803a0496d2267c051c8e8960240c6ce158f7f9743f'];
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
@@ -58,14 +61,16 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        privateKeys, 
+        `https://ropsten.infura.io/v3/c17dd0a089a64fdba9deff3469605c42`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
--- screencast/229-fork-uniswap/periphery/migrations/2_deploy_contracts.js
+++ screencast/229-fork-uniswap/periphery/migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const WETH = artifacts.require('WETH.sol');
 
 module.exports = async function (deployer, _network, addresses) {
   let weth;
-  const FACTORY_ADDRESS = '0x4298CC09c306B8bc0b3C231a9505C7B5632B6903';
+  const FACTORY_ADDRESS = '0x58E9966892546Ba3C9e5BcE02Fe16504aDB3b519';
 
   if(_network === 'mainnet') {
     weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
